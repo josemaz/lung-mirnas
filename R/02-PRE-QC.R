f42d2413c6747e023e39f79f0dee91be843b895d
Kevin Meza <kevinml@lcg.unam.mx>
2019-07-01 13:03:36 -0500

    actualizacion

--- R/02-PRE-QC.R
+++ R/02-PRE-QC.R
@@ -9,11 +9,11 @@ register(MulticoreParam(workers=detectCores()-1, progress=TRUE))
 cat("#################\n")
 cat("Step 2: QC\n")
 cat("#################\n")
-DATADIR <- 'pipeline/data/'
+DATADIR <- "pipeline/"
 RDATA <- paste(DATADIR, "rdata", sep="")
 PLOTSDIR <-paste(DATADIR, "plots", sep="")
 dir.create(PLOTSDIR)
-PLOTSDIR <-paste(DATADIR, "plots", sep = "")
+#PLOTSDIR <-paste(DATADIR, "plots", sep = "")
 PREDIR <- paste(PLOTSDIR, "QC_PRE", sep = "/")
 dir.create(PREDIR)
 w <- 1024 #Resolucion de los plots
@@ -27,15 +27,18 @@ load(file=paste(RDATA, "RawFull.RData", sep="/"))
 ###Let's keep only the GC & length annotated genes
 ##########################################
 ids<-!is.na(full$Annot$GC) & !is.na(full$Annot$Length)
+#ids es un vector de TRUE y FALSE que servirá para quedarse solo con las cuentas de aquellos genes cuya anotacion
+#tiene %GC y longitud del gene
 
-full$M<-full$M[ids,]
-full$Annot<-full$Annot[ids,]
+full$M<-full$M[ids,] #Aplicamos el filtro creado arriba y quitamos aquellas cuentas que no cumplan con los requisitos
+full$Annot<-full$Annot[ids,] #Aplicamos el filtro a las anotaciones y quitamos aquellas anotaciones que no cumplan con los requisitos
 cat("Non GC and lenght annotated genes removed.\n")
 
-row.names(full$M)<-full$Annot$EnsemblID
-row.names(full$Annot)<-full$AnnotdEnsemblID
-row.names(full$Targets)<-full$Targets$ID
-full$Targets$Group<-factor(substr(full$Targets$ID, start=1, stop=3))
+row.names(full$M)<-full$Annot$EnsemblID #Las cuentas te quedan organizaditas, el gen al que pertenecen, si son de sano o de enfermo y el numero de muestra
+row.names(full$Annot)<-full$Annot$EnsemblID #Ahora el numbre de las filas corresponde al ensembleID (lo tenemos 2 veces, una en el nombre de las filas y otra en la columna "esembleID")
+row.names(full$Targets)<-full$Targets$ID #El nombre de las filas ahora corresponde al ID (Ctr1, tBa2, etc...)
+full$Targets$Group<-factor(substr(full$Targets$ID, start=1, stop=3)) #subtr regresa Ctr y tBa de Ctr20 y tBa21, p.e.
+#En targets s crea un acolumna que indica si la muestra es de individuo sano o canceroso
 
 write.table(full$Targets, file=paste(RDATA, "Targets.tsv" , sep="/"), sep="\t", quote=FALSE, row.names=FALSE)
 save(full, file=paste(RDATA, "RawFull.RData", sep="/"), compress="xz")
@@ -61,6 +64,7 @@ png(filename=paste(PREDIR, "01-biodetection.Rd_%02d.png", sep="/"),  width=w, he
 explo.plot(mybiodetection, factor="Group" )
 dev.off()
 cat("Biodetection plots generated\n")
+#Se generan 2 plots (sanos y enfermos) que cotienen el porcentaje de "type" de los archivos 
 
 ## Count distribution per biotype. Using count per million, only for one sample
 mycountsbio <- dat(mydata, factor = NULL, type = "countsbio")
@@ -68,6 +72,7 @@ png(filename=paste(PREDIR, "02-countsbio.png", sep="/"), width=w, height=h, poin
 explo.plot(mycountsbio, toplot = 1, samples = 1, plottype = "boxplot")
 dev.off()
 cat("Counts distribution plot per biotype and one sample generated\n")
+#Se genera un plot donde se muestran valores de expresion de los "type"
 
 ## Count distribution per sample
 png(paste(PREDIR, "02-protein_coding_boxplot.png", sep="/"), width=w*2, height=h, pointsize=p)
@@ -75,11 +80,14 @@ explo.plot(mycountsbio, toplot = "protein_coding",
            samples = NULL, plottype = "boxplot")
 dev.off()
 cat("Counts distribution plot for protein coding and all samples generated\n")
+#Se genera un boxplot que muestra valores de expresion de mustras de "protein coding" de sanos y de enfermos
+
 png(paste(PREDIR, "02-protein_coding_barplot.png", sep="/"), width=w*2, height=h, pointsize=p)
 explo.plot(mycountsbio, toplot = "protein_coding",
            samples = NULL, plottype = "barplot")
 dev.off()
 cat("Counts distribution barplot for protein coding biotype and all samples generated\n")
+#Se genera un barplot que contiene CPM de genes "protein coding"  de sanos y de enfermos
 
 ## Count distribution per Experimental factors
 mycountsbio <- dat(mydata, factor = "Group", type = "countsbio")
@@ -88,6 +96,8 @@ explo.plot(mycountsbio, toplot = "protein_coding",
            samples = NULL, plottype = "boxplot")
 dev.off()
 cat("Counts distribution boxplot for protein coding biotype and group generated\n")
+#Se genera una grafica con 2 boxplots (sanos y enfermos) que grafican valores de expresion de "protein coding genes"
+
 png(paste(PREDIR, "04-protein_coding_barplot_group.png", sep="/"), width=w, height=h, pointsize=p)
 explo.plot(mycountsbio, toplot = "protein_coding",
            samples = NULL, plottype = "barplot")
@@ -112,7 +122,7 @@ dev.off()
 cat("GC bias plot generated\n")
 
 ## RNA composition
-# mycomp <- dat(mydata, type="cd")
+#mycomp <- dat(mydata, type="cd")
 # png(paste(PREDIR, "07-RNAComposition.png", sep="/"), width=w, height=h, pointsize=p)
 # explo.plot(mycomp, samples=1:12)
 # dev.off()
@@ -123,8 +133,7 @@ cat("GC bias plot generated\n")
 #########################
 # A complete pdf report can be obtained using this function.
 #¿No funciona con mas de 2 grupos?
-# QCreport(mydata, factor="Group", file=paste(PLOTSDIR, "QCReport.pdf", sep="/"))
-
+QCreport(mydata, factor="Group", file=paste(PLOTSDIR, "QCReport.pdf", sep="/"))
 
 
 #############################
@@ -132,7 +141,7 @@ cat("GC bias plot generated\n")
 ##########################################
 pca.dat <- dat(mydata, type = "PCA", logtransf = F)
 pca.results <- pca.dat@dat$result
- 
+
 ## Variance explained by each component
 pdf(file=paste(PREDIR, "08-PCAVariance_raw.pdf", sep="/"), width = 4*2, height = 4*2)
 barplot(pca.results$var.exp[,1], xlab = "PC", ylab = "Explained variance")
@@ -152,10 +161,8 @@ cat("PCA loading raw plot generated.\n")
 
 ## Score plot
 mycol <- as.character(full$Targets$Group)
-mycol[mycol == 'Ctr'] <- "black"
-mycol[mycol == 's1-'] <- "red2"
-mycol[mycol == 's2-'] <- "blue1"
-mycol[mycol == 's3-'] <- "green2"
+mycol[mycol == 'Ctr'] <- "red2"
+mycol[mycol == 'tBa'] <- "blue1"
 # mycol[mycol == 's4-'] <- "cyan2"
 
 pdf(file=paste(PREDIR, "10-PCAScore_raw.pdf", sep="/"), width = 5*2, height = 5)
@@ -171,9 +178,9 @@ plot(pca.results$scores[,1:2], col = "white",
      ylim = range(pca.results$scores[,1:2]) + 0.02*rango*c(-1,1))
 points(pca.results$scores[,1], pca.results$scores[,2], col = mycol, cex = 1.5)  
 # legend("topright", c("Ctr", "s1-", "s2-", "s3-", "s4-"), 
-legend("topright", c("Ctr", "s1-", "s2-", "s3-"), 
-  # col = c("black", "red2", "blue1", "green2", "cyan2"), ncol = 2, pch = 1)
-  col = c("black", "red2", "blue1", "green2"), ncol = 2, pch = 1)
+legend("topright", c("Ctr","tBa"), 
+       # col = c("black", "red2", "blue1", "green2", "cyan2"), ncol = 2, pch = 1)
+       col = c("red2", "blue1"), ncol = 2, pch = 1)
 
 # PC1 & PC3
 rango2 = diff(range(pca.results$scores[,c(1,3)]))
@@ -185,9 +192,9 @@ plot(pca.results$scores[,c(1,3)], col = "white",
      ylim = range(pca.results$scores[,c(1,3)]) + 0.02*rango2*c(-1,1))
 points(pca.results$scores[,1], pca.results$scores[,3], col = mycol, cex = 1.5)
 # legend("topright", c("Ctr", "s1-", "s2-", "s3-", "s4-"), 
-legend("topright", c("Ctr", "s1-", "s2-", "s3-"), 
-  # col = c("black", "red2", "blue1", "green2", "cyan2"), ncol = 2, pch = 1)
-  col = c("black", "red2", "blue1", "green2"), ncol = 2, pch = 1)
+legend("topright", c("Ctr", "tBA"), 
+       # col = c("black", "red2", "blue1", "green2", "cyan2"), ncol = 2, pch = 1)
+       col = c("red2", "blue1"), ncol = 2, pch = 1)
 dev.off()
 cat("PCA scores raw plot generated.\n")
 
@@ -197,3 +204,4 @@ cat("PCA scores raw plot generated.\n")
 cat("#################\n")
 cat("End of Step 2: QC\n")
 cat("#################\n")
+
