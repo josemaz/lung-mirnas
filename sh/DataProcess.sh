04e60ff0c40730ddc33d238540851acb234b9ea2
Kevin Meza <kevinml@lcg.unam.mx>
2019-07-05 11:36:39 -0500

    actualizacion

--- sh/DataProcess.sh
+++ sh/DataProcess.sh
@@ -13,5 +13,27 @@ echo '###############################################'
 echo '######## Pre-Quality Control & Plots ##########'
 echo '###############################################'
 echo
+
 Rscript --vanilla R/02-PRE-QC.R Adeno
 Rscript --vanilla R/02-PRE-QC.R Squamous
+
+echo
+echo '###############################################'
+echo '############## Normalization ##################'
+echo '###############################################'
+echo
+
+Rscript --vanilla R/03-NORM.R Adeno
+Rscript --vanilla R/03-NORM.R Squamous
+
+
+
+
+echo
+echo '###############################################'
+echo '########### END OF THE PROCESS ################'
+echo '###############################################'
+echo
+
+
+
