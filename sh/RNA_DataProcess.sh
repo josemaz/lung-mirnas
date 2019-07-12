4ae18be2ab50b8bbfe283abd434d47552321e798
kevinLCG <51832461+kevinLCG@users.noreply.github.com>
2019-07-12 12:32:11 -0500

    Update RNA_DataProcess.sh

--- sh/RNA_DataProcess.sh
+++ sh/RNA_DataProcess.sh
@@ -5,8 +5,12 @@ echo '########### Preprocessing RNA Data ###############'
 echo '##################################################'
 echo
 
+echo '--------------------- Adeno ----------------------'
 Rscript --vanilla R/01-Rectify_rnaSeq.R Adeno
+echo
+echo '------------------- Squamous ---------------------'
 Rscript --vanilla R/01-Rectify_rnaSeq.R Squamous
+echo
 
 echo
 echo '###############################################'
@@ -14,8 +18,12 @@ echo '######## Pre-Quality Control & Plots ##########'
 echo '###############################################'
 echo
 
+echo '--------------------- Adeno ----------------------'
 Rscript --vanilla R/02-PRE-QC.R Adeno
+echo
+echo '------------------- Squamous ---------------------'
 Rscript --vanilla R/02-PRE-QC.R Squamous
+echo
 
 echo
 echo '###############################################'
@@ -23,8 +31,12 @@ echo '############## Normalization ##################'
 echo '###############################################'
 echo
 
+echo '--------------------- Adeno ----------------------'
 Rscript --vanilla R/03-NORM.R Adeno
+echo
+echo '------------------- Squamous ---------------------'
 Rscript --vanilla R/03-NORM.R Squamous
+echo
 
 echo
 echo '###############################################'
@@ -32,8 +44,12 @@ echo '######## Pre-Quality Control & Plots ##########'
 echo '###############################################'
 echo
 
+echo '--------------------- Adeno ----------------------'
 Rscript --vanilla R/04-POST-QC.R Adeno
+echo
+echo '------------------- Squamous ---------------------'
 Rscript --vanilla R/04-POST-QC.R Squamous
+echo
 
 echo
 echo '###############################################'
