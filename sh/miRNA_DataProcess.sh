0d666f36e340b0f64816e5c1d2baef592a933200
kevinLCG <51832461+kevinLCG@users.noreply.github.com>
2019-07-12 12:34:34 -0500

    Update miRNA_DataProcess.sh

--- sh/miRNA_DataProcess.sh
+++ sh/miRNA_DataProcess.sh
@@ -5,8 +5,12 @@ echo '########### Preprocessing miRNA Data ###############'
 echo '####################################################'
 echo
 
+echo '--------------------- Adeno ----------------------'
 Rscript --vanilla R/miRNA-Normalize.R Adeno
+echo
+echo '------------------- Squamous ---------------------'
 Rscript --vanilla R/miRNA-Normalize.R Squamous
+echo
 
 echo
 echo '#####################################################'
